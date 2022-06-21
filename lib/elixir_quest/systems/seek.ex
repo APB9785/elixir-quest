3515b15e5a0622f92d4d56ec8ebae6b4aa931ca1
Andrew Berrien <74077809+APB9785@users.noreply.github.com>
2022-06-20 21:12:53 -0500

    Don't crash when mob targets die

--- lib/elixir_quest/systems/seek.ex
+++ lib/elixir_quest/systems/seek.ex
@@ -12,15 +12,19 @@ defmodule ElixirQuest.Systems.Seek do
 
     Enum.each(seeking, fn %{entity_id: mob_id, target_id: target_id} ->
       %{region_id: region_id, x: x, y: y} = Location.get(mob_id)
-      %{region_id: ^region_id, x: target_x, y: target_y} = Location.get(target_id)
 
-      # TODO: handle if target is no longer in the same region as the seeker
+      case Location.get(target_id) do
+        %{region_id: ^region_id, x: target_x, y: target_y} ->
+          direction = Utils.solve_direction({x, y}, {target_x, target_y})
 
-      direction = Utils.solve_direction({x, y}, {target_x, target_y})
+          # TODO: Look for alternate path if blocked?
 
-      # TODO: Look for alternate path if blocked?
+          Moving.add(entity_id: mob_id, direction: direction)
 
-      Moving.add(entity_id: mob_id, direction: direction)
+        _ ->
+          # target is no longer in the same region as the seeker
+          Seeking.remove(mob_id)
+      end
     end)
   end
 end
